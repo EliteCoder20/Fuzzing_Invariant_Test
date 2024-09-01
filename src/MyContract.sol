54c06996d87d992d20c21dc2ec654a8b2be18ca5
0xNerd <elitecoder43@gmail.com>
2024-09-01 17:50:48 +0530

    comments are imp

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -6,12 +6,13 @@ contract MyContract {
     uint private hiddenValue=0;
 
     function doStuff(uint data) public {
-        // if(data==3){
-        //     shouldAlwaysBeZero=10;
-        // }
-        // if(hiddenValue==7){
-        //      shouldAlwaysBeZero=10;
-        // }
+        if(data==3){
+            shouldAlwaysBeZero=10;   //fuzz test will fail when data==3
+
+        }
+        if(hiddenValue==7){
+             shouldAlwaysBeZero=10;      // invartiant will fail when hiddenValue =7
+        }
         hiddenValue=data;
     }
 }
